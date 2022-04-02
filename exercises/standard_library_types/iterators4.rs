ba985da978ce7ad9a98326fa1327331141240ab8
Oleksandr Pichkurov <oleksandr.pichkurov@getoxygen.com>
2022-04-02 14:16:50 +0300

    partial comleted

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,17 +1,12 @@
 // iterators4.rs
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    // let a = vec![1..=num].iter();
+    // a.next().map(|i| i + {i.next()}) as u64 # dont work
+    // (1..=num).sum1() # also dont work
+    // (1..=num).fold(1, |prev, next| prev * next) # work from standart lib
+    (1..=num).product()
 }
 
 #[cfg(test)]
